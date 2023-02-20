5c4dadb4922f31f342d2e23ca7e0f4b6aadc845e
Bree999777 <125664050+Bree999777@users.noreply.github.com>
2023-02-20 12:17:22 +0000

    Create pizza-restaurant-example.js
    This is for the Feedback Page to select Feedback

---
+++ pizza-restaurant-example.js
@@ -0,0 +1,55 @@
+ function showAnswer1(){
+        
+        
+        document.getElementById("verygood").value = "verygood";
+        document.getElementById("verygood").style.opacity="1";
+             document.getElementById("good").style.opacity=".5";
+                document.getElementById("average").style.opacity=".5";
+                    document.getElementById("bad").style.opacity=".5";
+                        document.getElementById("verybad").style.opacity=".5";
+    }
+        
+                    function showAnswer2(){
+        
+        
+        document.getElementById("good").value = "good";
+        document.getElementById("verygood").style.opacity=".5";
+             document.getElementById("good").style.opacity="1";
+                document.getElementById("average").style.opacity=".5";
+                    document.getElementById("bad").style.opacity=".5";
+                        document.getElementById("verybad").style.opacity=".5";
+    }
+        
+                    function showAnswer3(){
+        
+        
+        document.getElementById("average").value = "average";
+        document.getElementById("verygood").style.opacity=".5";
+             document.getElementById("good").style.opacity=".5";
+                document.getElementById("average").style.opacity="1";
+                    document.getElementById("bad").style.opacity=".5";
+                        document.getElementById("verybad").style.opacity=".5";
+    }
+        
+                    function showAnswer4(){
+        
+        
+        document.getElementById("bad").value = "bad";
+        document.getElementById("verygood").style.opacity=".5";
+             document.getElementById("good").style.opacity=".5";
+                document.getElementById("average").style.opacity=".5";
+                    document.getElementById("bad").style.opacity="1";
+                        document.getElementById("verybad").style.opacity=".5";
+    }
+        
+                    function showAnswer5(){
+        
+        
+        document.getElementById("verybad").value = "verybad";
+        document.getElementById("verygood").style.opacity=".5";
+             document.getElementById("good").style.opacity=".5";
+                document.getElementById("average").style.opacity=".5";
+                    document.getElementById("bad").style.opacity=".5";
+                        document.getElementById("verybad").style.opacity="1";
+    }
+        
